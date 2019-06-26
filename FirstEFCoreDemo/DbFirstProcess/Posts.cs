fc73e36e8f420892334101ef8e3a1ab6a9eeea8d
zhiwen <zhiwen@kooboo.cn>
2019-06-26 18:24:17 +0800

    DBFirst反向生成DbContext和Model

---
+++ FirstEFCoreDemo/DbFirstProcess/Posts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstEFCoreDemo.DbFirstProcess
+{
+    public partial class Posts
+    {
+        public int PostId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public virtual Blogs Blog { get; set; }
+    }
+}
---
+++ FirstEFCoreDemo/DbFirstProcess/bloggingContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FirstEFCoreDemo.DbFirstProcess
+{
+    public partial class bloggingContext : DbContext
+    {
+        public bloggingContext()
+        {
+        }
+
+        public bloggingContext(DbContextOptions<bloggingContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Blogs> Blogs { get; set; }
+
+        public virtual DbSet<Posts> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySQL("Server=47.100.220.174;Database=blogging;User Id=root;Password=LZWxm!@#123456");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+
+            modelBuilder.Entity<Blogs>(entity =>
+            {
+                entity.HasKey(e => e.BlogId);
+
+                entity.ToTable("Blogs", "blogging");
+
+                entity.Property(e => e.BlogId).HasColumnType("int(11)");
+
+                entity.Property(e => e.Url).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Posts>(entity =>
+            {
+                entity.HasKey(e => e.PostId);
+
+                entity.ToTable("Posts", "blogging");
+
+                entity.HasIndex(e => e.BlogId);
+
+                entity.Property(e => e.PostId).HasColumnType("int(11)");
+
+                entity.Property(e => e.BlogId).HasColumnType("int(11)");
+
+                entity.Property(e => e.Content).IsUnicode(false);
+
+                entity.Property(e => e.CreateDate).HasDefaultValueSql("0001-01-01 00:00:00");
+
+                entity.Property(e => e.Title).IsUnicode(false);
+
+                entity.HasOne(d => d.Blog)
+                    .WithMany(p => p.Posts)
+                    .HasForeignKey(d => d.BlogId);
+            });
+        }
+    }
+}
